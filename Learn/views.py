1480edafc9245d342dff787d679ae542d41cc7ad
Mr-Kai23 <zk19950923>
2020-07-28 13:32:09 +0800

    上传

--- Learn/views.py
+++ Learn/views.py
@@ -5,11 +5,18 @@ import json
 
 # Create your views here.
 
+# 对用户Token的验证
+class TokenAuthentication:
+    def
+
+
 
 # APIView 继承的 View
 class StudentViews(APIView):
 
     def get(self, request):
+        # # 源码阅读从dispatch()开始，将原始request丰富了新的属性，在属性中进行了用户验证
+        self.dispatch()
 
         res = {
             'code': 1000,
@@ -17,3 +24,15 @@ class StudentViews(APIView):
         }
 
         return HttpResponse(json.dumps(res), status=201)
+
+    def post(self, request):
+
+        return HttpResponse('创建Student')
+
+    def put(self, request):
+
+        return HttpResponse('修改Student')
+
+    def delete(self, request):
+
+        return HttpResponse('删除Student')
