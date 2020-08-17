524e9b3840b1c0e55d2e3853857f81cc540f2d35
Mr-Kai23 <zk19950923>
2020-08-17 22:45:46 +0800

    上传

--- Learn/role_views.py
+++ Learn/role_views.py
@@ -37,7 +37,43 @@ class RoleView(APIView):
 # 分页视图
 # ===========================================================
 from .serializers import PagerSerializer
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination, CursorPagination
+
+
+class MyPageNumberPagination(PageNumberPagination):
+    """
+    自定义分页配置类
+    分页，看第n页，每页显示n条数据
+    """
+    page_size = 2
+    page_size_query_param = 'size'  # 路由中分页可传的参数，表示可以传一个size的参数，每个页面的数据量
+    max_page_size = 8  # 最大每页数据数量
+
+    page_query_param = 'page'  # 传入的页码参数
+
+
+class MyLimitOffsetPagination(LimitOffsetPagination):
+    """
+    自定义分页配置类
+    分页，在n个位置，向后查看n条数据
+    """
+    default_limit = 2
+    limit_query_param = 'limit'  # 每个页面的限定数据量参数
+    max_limit = 8  # 最大每页数据数量
+
+    offset_query_param = 'offset'  # 传入的起始数据的索引参数
+
+
+class MyCursorPagination(CursorPagination):
+    """
+    自定义分页配置类
+    分页，加密分页，上一页下一页
+    """
+    cursor_query_param = 'cursor'
+    page_size = 2
+    max_page_size = 8  # 最大每页数据数量
+
+    page_size_query_param = None
 
 
 class Pager1View(APIView):
@@ -49,11 +85,23 @@ class Pager1View(APIView):
         roles = Role.objects.all()
 
         # 分页实例
-        pagination = PageNumberPagination()
+        # 1.
+        # pagination = PageNumberPagination()  # page参数：页码，size参数：单页数据量
+        # pagination = MyPageNumberPagination()
+        # 2.
+        # pagination = LimitOffsetPagination()  # offset参数：起始数据的索引，limit参数：单页面限定展示的数据量
+        # pagination = MyLimitOffsetPagination()
+        # 3.
+        # pagination = CursorPagination()
+        pagination = MyCursorPagination()
+
         # 传入 queryset 、 请求 对象 和 视图对象（也就是当前视图对象，self）(可写可不写)
         pager_roles = pagination.paginate_queryset(queryset=roles, request=request, view=self)
 
         # 序列化分页后的数据
         serializer = PagerSerializer(instance=pager_roles, many=True)
 
-        return Response(serializer.data)
+        # 返回分页自动生成的响应,第1、2种可以适用两种返回形式，第3种分页，只适合以下返回形式
+        return pagination.get_paginated_response(data=serializer.data)
+
+        # return Response(data=serializer.data)
